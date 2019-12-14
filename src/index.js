99fef3860609ad5a78aca5994fa59125ece363c6
rbarakhvostov <rbarakhvostov@gmail.com>
2019-12-14 20:55:46 +0300

    add fully completed counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log(1234)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
